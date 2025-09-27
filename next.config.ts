ced71630f631de32bb6a236722d85e5f5668330c
Oscarin2001 <elmataperras392@gmail.com>
2025-09-27 04:16:59 -0400

    build: ignore libsql README/LICENSE files to fix build parse errors

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,21 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: [
     "https://generativelanguage.googleapis.com",
   ],
+  webpack: (config) => {
+    // Evitar que Webpack intente parsear README.md / LICENSE de paquetes libsql que se resuelven vía exports
+    // Opción 1: tratarlos como 'asset/source'
+    config.module.rules.push({
+      test: /node_modules\\\\@?(libsql|prisma).*\\\\(README|LICENSE).*$/i,
+      type: 'asset/source'
+    });
+    // Opción 2: ignora patrones específicos (por seguridad adicional)
+    const IgnorePlugin = require('webpack').IgnorePlugin;
+    config.plugins.push(new IgnorePlugin({
+      resourceRegExp: /(README\.md|LICENSE)$/,
+      contextRegExp: /@libsql/
+    }));
+    return config;
+  }
 };
 
 export default nextConfig;
